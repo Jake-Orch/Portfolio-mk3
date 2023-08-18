8520683872e145164a16d0c363831daf827c4c59
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-08-18 15:33:46 +0100

    data added

--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ import {BiHomeAlt2, BiSolidHomeAlt2} from "react-icons/bi";
 import {BsPerson, BsFillPersonFill} from "react-icons/bs";
 import {AiOutlineProject, AiFillProject, AiOutlineMail, AiFillMail} from "react-icons/ai";
 import {FiDownload} from "react-icons/fi"
+import images from "./assets/projects";
 
 const cName = "nav-text";
 
@@ -42,3 +43,47 @@ export const sidebarData = [
     }
 ]
 
+export const projects = [
+    {
+        name: "WizDumb",
+        img: images.WizDumb,
+        alt: "WizDumb Homepage",
+        deployed: "https://wizdumb-app-a832fc305640.herokuapp.com/",
+        git: "https://github.com/Cdrcar/Wizdumb",
+      },
+      {
+        name: "Safe Keeping",
+        img: images.SafeKeeping,
+        alt: "Safe Keeping Homepage",
+        deployed: "https://safekeeping.herokuapp.com/",
+        git: "https://github.com/Jake-Orch/Project2",
+      },
+      {
+        name: "Weather I Go",
+        img: images.WeatherIGo,
+        alt: "Weather I Go Homepage",
+        deployed: "https://faithscoding.github.io/weather-i-go/",
+        git: "https://github.com/FaithsCoding/weather-i-go",
+      },
+      {
+        name: "Note Maker PWA",
+        img: images.NoteMakerPWA,
+        alt: "Note Maker PWA Homepage",
+        deployed: "https://note-maker-pwa-48cf86482d7b.herokuapp.com/",
+        git: "https://github.com/Jake-Orch/myNoteMakerPWA",
+      },
+      {
+        name: "Weather Dashboard",
+        img: images.WeatherDashboard,
+        alt: "Weather Dashboard Homepage",
+        deployed: "https://jake-orch.github.io/Weather-dashboard/",
+        git: "https://github.com/Jake-Orch/Weather-dashboard",
+      },
+      {
+        name: "Quiz",
+        img: images.Quiz,
+        alt: "Quiz Homepage",
+        deployed: "https://jake-orch.github.io/Quiz/",
+        git: "https://github.com/Jake-Orch/Quiz",
+      },
+]
