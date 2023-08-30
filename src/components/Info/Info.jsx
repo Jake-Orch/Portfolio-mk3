1737acb0a9c4f8d9172783fa0f91733a3d895940
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-08-30 20:27:41 +0100

    tech stack added

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,14 +1,29 @@
 import React from "react";
-import { info } from "../../data";
+import { info, icons } from "../../data";
 import "./info.css";
 
 const Info = () => {
-    return (
-        <div className="info_container">
-            <h2>{info.tagline}</h2>
-            <p>{info.desc}</p>
+  return (
+    <div className="info_container">
+      <div className="info_desc">
+        <h2>{info.tagline}</h2>
+        <p>{info.desc}</p>
+      </div>
+      <div className="info_stack">
+        <h3>Tech Stack</h3>
+        <div className="info_icons">
+          {icons.map((icon) => (
+            <img
+              id={"info_" + icon.cl}
+              src={icon.logo}
+              title={icon.title}
+              alt={icon.title}
+            />
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Info;
