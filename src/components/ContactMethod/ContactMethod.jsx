256d393dbe1421c2df8d9b599d7b5b0dc7e61d10
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-09-10 15:23:14 +0100

    contactmethod component created

--- src/components/ContactMethod/ContactMethod.jsx
+++ src/components/ContactMethod/ContactMethod.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./contactmethod.css";
 
-const ContactMethod = ({ num, title, msg, icon }) => {
+const ContactMethod = ({ num, title, msg, icon, value }) => {
   return (
     <div className="contactm_container">
       <div className="contactm_icon">{icon}</div>
       <div className="contactm_body">
         <h3>{title}</h3>
         <p>{msg}</p>
+        <p className="contactm_value">{value}</p>
       </div>
     </div>
   );
--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -7,18 +7,22 @@ const Contact = () => {
   return (
     <div className="contact_container">
       <Title name={"Contact"} />
-      <div className="contact_form">
-        <Form />
-
-        {/* {contactInfo.map((info, index) => (
-          <ContactMethod
-            key={index}
-            num={index}
-            title={info.title}
-            msg={info.msg}
-            icon={info.icon}
-          />
-        ))} */}
+      <div className="contact_body">
+        <div className="contact_form">
+          <Form />
+        </div>
+        <div className="contact_methods">
+          {contactInfo.map((info, index) => (
+            <ContactMethod
+              key={index}
+              num={index}
+              title={info.title}
+              msg={info.msg}
+              icon={info.icon}
+              value={info.value}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
