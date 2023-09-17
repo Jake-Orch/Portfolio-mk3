8f3feb0db61d99e09a2cd3affba6f1bfd69183b4
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-09-17 21:04:01 +0100

    home element resizing

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,12 +3,14 @@ import { Portrait, Welcome } from "../../components";
 import "./home.css";
 
 const Home = () => {
-    return (
-        <div className="container home_container">
-            <Welcome />
-            <Portrait cl={"home_portrait"} />
-        </div>
-    )
+  return (
+    <div className="container home_container">
+      <div className="home_body">
+        <Welcome />
+        <Portrait cl={"home_portrait"} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
