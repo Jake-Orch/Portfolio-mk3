2d95456e0ea13e14fa4b79b15d35227297712c8d
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-09-18 20:01:42 +0100

    github and linkedin links added to homepage

--- src/components/Portrait/Portrait.jsx
+++ src/components/Portrait/Portrait.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import Me from "../../assets/me.png"
-import "./portrait.css"
+import Me from "../../assets/me.png";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import "./portrait.css";
 
-const Portrait = ({cl}) => {
-    return (
-        <div className={cl}>
-            <img className="home_portrait_image" src={Me} alt="portrait"></img>
-        </div>
-    )
-}
+const Portrait = ({ cl }) => {
+
+  const handleMyGit = () => {
+    window.open("https://github.com/Jake-Orch")
+  }
+
+  const handleMyLinkedin = () => {
+    window.open("https://www.linkedin.com/in/jake-orchard-dev")
+  }
+  return (
+    <div className={cl}>
+      <img className="home_portrait_image" src={Me} alt="portrait"></img>
+      <div className="home_links">
+        <AiFillGithub title="GitHub" onClick={handleMyGit}/>
+        <AiFillLinkedin title="Linkedin" onClick={handleMyLinkedin}/>
+      </div>
+    </div>
+  );
+};
 
 export default Portrait;
