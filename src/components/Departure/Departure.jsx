1bd92cd95171e570c0ce98fd5c091b849585947a
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-09-25 18:12:25 +0100

    departure added

---
+++ src/components/Departure/Departure.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./departure.css";
+
+const Departure = ({cl}) => {
+  return (
+    <div className={cl}>
+      <h2>Thank You</h2>
+      <p>
+        Thank you for taking the time to look at my portfolio and to consider me
+        for your available role. I look forward to hearing from you! <br></br> Have a
+        nice day.
+      </p>
+    </div>
+  );
+};
+
+export default Departure;
